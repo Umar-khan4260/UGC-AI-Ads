5ce32bbc062c44b01378c6e28e27ff0b1b55d90c
umarKhan <umar68408@gmail.com>
2026-09-07 16:10:32 +0500

    now fixing the neon db isssue and also create the user controller

--- server/server.ts
+++ server/server.ts
@@ -1,21 +1,44 @@
-import cors from 'cors';
-import express, { Request, Response } from 'express';
-import 'dotenv/config';
-import { clerkMiddleware } from '@clerk/express'
+import "./configs/instrument.mjs";
+import cors from "cors";
+import express, { Request, Response } from "express";
+import "dotenv/config";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhooks from "./controllers/clerk.js";
+import * as Sentry from "@sentry/node"
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 
-// Middleware 
-app.use(clerkMiddleware())
+
+// Middleware
+
+
 app.use(cors());
+
+app.post(
+  "/api/clerk",
+  express.raw({ type: "application/json" }),
+  clerkWebhooks,
+);
+
 app.use(express.json());
+app.use(clerkMiddleware());
 
 const PORT = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Server is Live!');
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is Live!");
 });
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+app.use('/api/user',userRouter)
+
+// The error handler must be registered before any other error middleware and after all controllers
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
