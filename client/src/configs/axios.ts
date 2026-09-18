7becb21e0c1a1cbad51040b133a9d9e25206cd52
umarKhan <umar68408@gmail.com>
2026-09-18 15:59:43 +0500

    update the url with live backend

--- client/src/configs/axios.ts
+++ client/src/configs/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({    
-    baseURL: import.meta.env.VITE_BASEURL || 'http://localhost:5000'
+    baseURL: import.meta.env.VITE_BASEURL || 'https://ugc-ai-ads.onrender.com'
 })
 
 export default api;
